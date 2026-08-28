1ca23023f1ad6530a8a5dd50536a627aef5275c1
moinul <moinulibr@gmail.com>
2026-08-28 23:10:14 +0600

    setting route define

--- routes/api.php
+++ routes/api.php
@@ -102,5 +102,6 @@ Route::prefix('v1/app')->group(function () {
 
         //setting static data [temporary]
         Route::get('/get-setting-data', [SettingsController::class, 'getSettingData']);
+        Route::get('/delivery-charge-info',[SettingsController::class, 'getDeliveryChargeInfo']);   
     });
 });
