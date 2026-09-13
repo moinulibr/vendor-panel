ba2ccd987264d7782850022602a57bf28187fac7
moinul <moinulibr@gmail.com>
2026-09-13 12:20:25 +0600

    firebase env setup

--- config/firebase.php
+++ config/firebase.php
@@ -50,8 +50,8 @@ return [
              *
              */
 
-            'credentials' => env('FIREBASE_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
-
+            //'credentials' => env('FIREBASE_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
+            'credentials' => storage_path('app/' . env('FIREBASE_CREDENTIALS', 'firebase/firebase_credentials.json')),
             /*
              * ------------------------------------------------------------------------
              * Firebase Auth Component
